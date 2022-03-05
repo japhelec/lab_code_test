53badb6f79bc4911e95e28ffbccfae0e1ba08117
kueikao <r08921109@ntu.edu.tw>
2022-03-05 17:34:17 +0800

    feat: simple UDP client and server

---
+++ udp/client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import socket
+
+HOST = '0.0.0.0'
+PORT = 5566
+server_addr = (HOST, PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    outdata = raw_input('please input message: ')
+    print('sendto ' + str(server_addr) + ': ' + outdata)
+    s.sendto(outdata.encode(), server_addr)
+    
+    indata, addr = s.recvfrom(1024)
+    print('recvfrom ' + str(addr) + ': ' + indata.decode())
