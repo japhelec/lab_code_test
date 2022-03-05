f153fd9cfcc86f473f1bb53dd6d450df52fbc0ae
kueikao <r08921109@ntu.edu.tw>
2022-03-05 10:25:26 +0800

    feat: custom key for pre-experiment interaction

--- tello/main.py
+++ tello/main.py
@@ -9,6 +9,7 @@ import keyboard
 
 
 keepread = True
+isControl = False
 drone = tello.Tello('', 8889)  
 camera = Camera()
 
@@ -20,25 +21,56 @@ def recvImage():
             continue 
         
         LP_pose = pose_estimation(frame, camera)
-        control(drone, LP_pose)
-        # print(LP_pose)
+
+        if (isControl):
+            control(drone, LP_pose)
 
     cv2.destroyAllWindows()
 
 # keyborad
 def recvkeybord(): 
     while True:
-        if keyboard.is_pressed('q'): #quit
+        key = keyboard.read_key()
+
+        if key == "o": #takeoff
+            drone.send_command("takeoff")
+            time.sleep(2)
+        elif key == "v": #vision
+            camera.switch_vision()
+            drone.send_command("downvision " + str(camera.vision.value))
+            time.sleep(1)
+        elif key == "i": #control switch
+            isControl = not isControl
+            time.sleep(1)
+        
+
+        elif key == "w": #move forward
+            drone.send_command("rc 0 30 0 0")
+            time.sleep(1)
+        elif key == "a": #move left
+            drone.send_command("rc -30 0 0 0")
+            time.sleep(1)
+        elif key == "s": #move backward
+            drone.send_command("rc 0 -30 0 0")
+            time.sleep(1)
+        elif key == "d": #pause right
+            drone.send_command("rc 30 0 0")
+            time.sleep(1)
+        elif key == "q": #move up
+            drone.send_command("rc 0 0 30 0")
+            time.sleep(1)
+        elif key == "e": #move down
+            drone.send_command("rc 0 0 -30 0")
+            time.sleep(1)
+        elif key == "p": #pause
+            drone.send_command("stop")
+            time.sleep(1)
+
+        elif key == "l": #land
+            drone.send_command("land")
             global keepread
             keepread = False
             break
-        if keyboard.is_pressed('v'): #vision
-            camera.switch_vision()
-            drone.send_command("downvision " + str(camera.vision.value))
-            time.sleep(2)
-        if keyboard.is_pressed('s'): #stop
-            drone.send_command("stop")
-            time.sleep(1)
 
 
 
