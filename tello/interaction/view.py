040279f3208c3f1ac0f584c8555d637eb98d9e32
kueikao <r08921109@ntu.edu.tw>
2022-04-12 21:16:53 +0800

    style: save video functionality migrated to action

--- tello/interaction/view.py
+++ tello/interaction/view.py
@@ -4,7 +4,6 @@ from threading import Thread
 
 class View:
     def __init__(self, drone):
-        self.toSave = False
         self.drone = drone
 
         self.thread = Thread(target=self._stream_thread)
@@ -16,12 +15,7 @@ class View:
         while True:
             frame = self.drone.read()
             if frame is None or frame.size == 0:
-                # add black frame
                 continue 
             cv2.imshow("frame",frame)
             cv2.waitKey(1)
         cv2.destroyAllWindows()
-
-
-    def set_save_mode(self, mode):
-        self.toSave = mode
